79840d3cd7b5cb765755d0138ab6b4a024ee6b37
kchoi <kay.n.choi@gmail.com>
2013-10-20 18:50:24 -0500

    created empty project that uses postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Csc667867TeamYak::Application.config.secret_key_base = '5e4110fa9fc56d8a5b61e2578e1b00977654a28b6e5e395c5aba5bbdaf24e10566ef7360a01ceac64a986fafa81eebbe5e8f41cd962bec83dc769374da32266b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Csc667867TeamYak::Application.config.session_store :cookie_store, key: '_csc-667-867-team-yak_session'
