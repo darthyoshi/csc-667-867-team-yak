3baa658a735b3cf69f7dd6b684144d6e1849518c
Yulia Falls <ioulia@mail.sfsu.edu>
2013-10-26 23:53:45 -0700

    Added "Your Account" link

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Csc667867TeamYak::Application.routes.draw do
+  root to: 'welcome#index'
+  get "account/index"
   devise_for :users
   resources :art_pieces
-  
-  #get "welcome/index" 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
-  root to: 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -58,4 +57,5 @@ Csc667867TeamYak::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
