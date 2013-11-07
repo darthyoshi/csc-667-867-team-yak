f6f83b28ea2d4985ba93cad344becc00985eeb16
Kay Choi <kay.n.choi@gmail.com>
2013-11-06 21:58:28 -0800

    - consolidated results/category and results/search views into results/show view  - added <hr> element to results/show view

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,12 +1,28 @@
 class ResultsController < ApplicationController
   @artworks
+  @title
+
+  def show
+  end
+
   def search
     @query = params[:search].split(/[\s.]/)
     @artworks = Artwork.where("title = ?", @query[0])
+    @title = "Search Results"
+
+    render'show'
   end
 
   #GET /category/:cat
   def category
     @artworks = Artwork.where("category = ?", params[:cat])
+    CATEGORIES.each do |cat,details|
+      if details['url'] == request.fullpath
+        @title = details['description']
+        break
+      end
+    end
+
+    render 'show'
   end
 end
