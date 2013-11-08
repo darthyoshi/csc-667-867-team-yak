f0c03ff2d319c16527d9a9d94b08054fe9c94612
Yulia Falls <ioulia@mail.sfsu.edu>
2013-11-08 15:44:53 -0800

    admin for artworks improved

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
     # if user_signed_in?
     
     #@artworks = Artwork.all
-    @artworks = Artwork.paginate(:page => params[:page], :per_page => 10)
+    @artworks = Artwork.paginate(:page => params[:page], :per_page => 8)
   end
   
   def show
