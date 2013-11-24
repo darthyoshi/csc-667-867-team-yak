28e76e5b38c75bfd9fdfa1e124146d0099aa09b4
kchoi <kay.n.choi@gmail.com>
2013-11-24 02:28:49 -0800

    - minor CSS tweak to results pages (tags will stay aligned)  - clicking on tags in the results will run a search by tags  - results pages now list item seller names  - clicking on item seller name on results pages will run a search by seller name  - search type can be specified using <type>:<search> format  - results are ordered by creation date, then alphanumerically  - TODO: currently omitting the format searches only item names by default, will need to change to search everything

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,6 @@
 class ResultsController < ApplicationController
+#  require 'fuzzystringmatch'
+
   @artworks
   @title
 
@@ -6,43 +8,38 @@ class ResultsController < ApplicationController
   end
 
   def search
+#    jarow = FuzzyStringMatch::JaroWinkler.create(:native)
     page = if params[:page].nil? then 1 else params[:page] end
-    terms = params[:search].split(':')
-=begin
-    tag_search = Array.new
-    title_search = Array.new
-    seller_search = Array.new
-    desc_search = Array.new
+    terms = params[:search].split(':', 2)
 
-    terms.each_with_index do |term, index|
-        if term == "title"
-            title_search.push (index + 1)
-        elsif term == "tag"
-            tag_search.push (index + 1)
-        elsif term == "seller"
-            seller_search.push (index + 1)
-        elsif term == "desc"
-            desc_search.push (index + 1)
-        end
+    if terms[0] == "name"
+      @artworks = Artwork.where("lower(title) like lower(?)", '%'+terms[1]+'%').order("created_at desc, title asc").paginate(:page => page, :per_page => 10)
+    elsif terms[0] == "tag"
+      @artworks = Artwork.joins(:taggings).joins(:arttags).distinct.where("lower(tagname) like lower(?)", '%'+terms[1]+'%').order("created_at desc, title asc").paginate(:page => page, :per_page => 10)
+    elsif terms[0] == "desc"
+      @artworks = Artwork.where("lower(description) like lower(?)", '%'+terms[1]+'%').order("created_at desc, title asc").paginate(:page => page, :per_page => 10)
+    elsif terms[0] == "seller"
+      @artworks = Artwork.joins(:seller).distinct.where("lower(displayed_name) like lower(?)", '%'+terms[1]+'%').order("created_at desc, title asc").paginate(:page => page, :per_page => 10)
+    else
+      @artworks = Artwork.where("lower(title) like lower(?)", '%'+params[:search]+'%').order("created_at desc, title asc").paginate(:page => page, :per_page => 10)
     end
-=end
-    @artworks = Artwork.where("id > ?", @query[0]).paginate(:page => page, :per_page => 10)
+    
     @title = "Search Results"
 
     render'show'
   end
 
-  #GET /category/:cat
+  #GET /browse/:cat
   def category
     page = if params[:page].nil? then 1 else params[:page] end
     if params[:cat] != "all"
-        @artworks = Artwork.where("category = ?", params[:cat]).paginate(:page => page, :per_page => 10)
+        @artworks = Artwork.where("category = ?", params[:cat]).order("created_at desc, title asc").paginate(:page => page, :per_page => 10)
     else
-        @artworks = Artwork.available.paginate(:page => params[:page], :per_page => 10)
+        @artworks = Artwork.all.order("created_at desc, title asc").paginate(:page => params[:page], :per_page => 10)
     end
     CATEGORIES.each do |cat,details|
       if request.fullpath.include? details['url']
-        @title = "Artworks by category: " + details['description']
+        @title = "Artworks: " + details['description']
         break
       end
     end
